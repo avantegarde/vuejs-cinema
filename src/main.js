86fd4115c36c64ad4e3612a94022ecc1ea8cbace
kaelsteinert <kaelsteinert@gmail.com>
2020-02-10 15:46:10 -0500

    split code into separate vue template files. started pulling data from api

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import  Vue from 'vue';
-import './style.css';
+import css from './style.css';
+
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
+
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
 new Vue({
   el: "#app",
-  data:{
-    msg: "Hello World"
+  data: {
+    genre: [],
+    time: [],
+    movies: []
+  },
+  methods: {
+    checkFilter(cat,title,checked){
+      if(checked){
+        this[cat].push(title);
+      } else {
+        let index  = this[cat].indexOf(title);
+        if(index > -1){
+          this[cat].splice(index, 1);
+        }
+      }
+    }
+  },
+  components:{
+    MovieList,
+    MovieFilter
+  },
+  created(){
+    this.$http.get('/api').then(response => {
+      this.movies = response.data;
+    });
   }
 });
